8ec6a855baa6de11094ed27c9085f57bdb0d0fda
RafaelLeone <rinaciodacosta@gmail.com>
2022-08-04 15:47:26 -0300

    seila

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ import random
 
 # Create your views here.
 def app_index(request):
+    
     for i in range(6):
         numero = random.randint(1, 905)
         url = f'https://pokeapi.co/api/v2/pokemon/{numero}'
@@ -14,8 +15,17 @@ def app_index(request):
         foto = resp.json()['sprites']['front_default']
         Pokemon.objects.create(nome=nome, foto=foto)
     pokes = Pokemon.objects.all().order_by('-id')[:6]
+    for i in range(6):
+        numero = random.randint(1, 905)
+        url = f'https://pokeapi.co/api/v2/pokemon/{numero}'
+        resp = requests.get(url)
+        nome = resp.json()['name']
+        foto = resp.json()['sprites']['front_default']
+        Pokemon.objects.create(nome=nome, foto=foto)
+    pokez = Pokemon.objects.all().order_by('-id')[6:12]
     contexto = {
-        'pokes' : pokes    
+        'pokes' : pokes,
+        'pokez' : pokez 
     }
 
     return render(request, 'app/index.html', contexto)
