ded84215b0ca3768ce2a1a4f8385b8a69b67dd8f
RafaelLeone <rinaciodacosta@gmail.com>
2022-08-08 19:17:34 -0300

    pytest e flake8 no GA?

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -14,7 +14,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Pokemon',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                    )),
                 ('nome', models.CharField(max_length=255)),
                 ('foto', models.URLField()),
             ],
--- app/views.py
+++ app/views.py
@@ -6,26 +6,26 @@ import random
 
 # Create your views here.
 def app_index(request):
-    
+    pokes = []
+    pokez = []
     for i in range(6):
         numero = random.randint(1, 905)
         url = f'https://pokeapi.co/api/v2/pokemon/{numero}'
         resp = requests.get(url)
         nome = resp.json()['name']
         foto = resp.json()['sprites']['front_default']
-        Pokemon.objects.create(nome=nome, foto=foto)
-    pokes = Pokemon.objects.all().order_by('-id')[:6]
+        poke = Pokemon(nome=nome, foto=foto)
+        pokes.append(poke)
     for i in range(6):
         numero = random.randint(1, 905)
         url = f'https://pokeapi.co/api/v2/pokemon/{numero}'
         resp = requests.get(url)
         nome = resp.json()['name']
         foto = resp.json()['sprites']['front_default']
-        Pokemon.objects.create(nome=nome, foto=foto)
-    pokez = Pokemon.objects.all().order_by('-id')[6:12]
+        poke = Pokemon(nome=nome, foto=foto)
+        pokez.append(poke)
     contexto = {
-        'pokes' : pokes,
-        'pokez' : pokez 
+        'pokes': pokes,
+        'pokez': pokez
     }
-
     return render(request, 'app/index.html', contexto)
